3eb91b443aea54916be123944702cbfcff6d5147
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-23 16:26:38 -0500

    Basic message read from the Programmatic queue

---
+++ src/main/java/com/rabbitMQ/listener/RabbitMQListener/RabbitMQMessageListener.java
@@ -0,0 +1,17 @@
+package com.rabbitMQ.listener.RabbitMQListener;
+
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageListener;
+
+/**
+ * Created by nichaurasia on Thursday, January/23/2020 at 1:41 PM
+ */
+
+public class RabbitMQMessageListener implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        String receivedMessage = new String(message.getBody());
+        System.out.println("message = " + receivedMessage);
+
+    }
+}
