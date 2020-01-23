646c9a2cbca47c570d18b794e07cb16cc793eb92
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-23 13:39:29 -0500

    first commit

---
+++ src/test/java/com/rabbitMQ/listener/RabbitMQListener/RabbitMqListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitMQ.listener.RabbitMQListener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
