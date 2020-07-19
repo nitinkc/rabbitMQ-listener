2aa2b2939b59f7bfa208c2f5284c3bb61495aadc
Nitin Chaurasia <gs.nitin@gmail.com>
2020-07-19 10:18:43 -0400

    Update RabbitMQMessageListener.java

--- src/main/java/com/rabbitMQ/listener/RabbitMQListener/RabbitMQMessageListener.java
+++ src/main/java/com/rabbitMQ/listener/RabbitMQListener/RabbitMQMessageListener.java
@@ -2,16 +2,21 @@ package com.rabbitMQ.listener.RabbitMQListener;
 
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * Created by nichaurasia on Thursday, January/23/2020 at 1:41 PM
  */
 
 public class RabbitMQMessageListener implements MessageListener {
+	
+	@Value("${rabbitmq.hostname}")
+    private String hostName;
+	
     @Override
     public void onMessage(Message message) {
         String receivedMessage = new String(message.getBody());
         System.out.println("message = " + receivedMessage);
-
+        System.out.println(hostName);
     }
 }
